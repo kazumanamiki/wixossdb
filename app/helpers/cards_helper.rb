50c42e3810b243c544b26b39f3d1abdcc1bb0ede
nami <kazuma.namiki@outlook.jp>
2014-05-28 22:08:23 +0900

    カードの色を分かりやすくする

--- app/helpers/cards_helper.rb
+++ app/helpers/cards_helper.rb
@@ -3,4 +3,42 @@ module CardsHelper
 		return "—" if data.blank?
 		return data
 	end
+
+	def color_string(color)
+		case color
+		when "赤"
+			return "#ECA5B2"
+		when "青"
+			return "#58B6D2"
+		when "緑"
+			return "#387948"
+		when "白"
+			return "#FFF5EE"
+		when "黒"
+			return "#767676"
+		when "無"
+			return "#FFFFFF"
+		else
+			return "#FFFFFF"
+		end
+	end
+
+	def color_image_file(color)
+		case color
+		when "赤"
+			return "i_red.png"
+		when "青"
+			return "i_blue.png"
+		when "緑"
+			return "i_green.png"
+		when "白"
+			return "i_white.png"
+		when "黒"
+			return "i_black.png"
+		when "無"
+			return "i_null.png"
+		else
+			return ""
+		end
+	end
 end
